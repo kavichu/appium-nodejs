6ef7373c4532ec0ee0c608b4437169a6c984fa04
Luis Valdes <luischivaldes@gmail.com>
2016-07-23 18:43:29 -0400

    Trigger build.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const os = require("os");
 
 // Constants
 const PORT = 8080;
@@ -8,7 +9,7 @@ const PORT = 8080;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world, I am running inside a Docker container.\n');
+  res.send(`Hello world, I am running inside a Docker container ${os.hostname()}.\n`);
 });
 
 app.listen(PORT);
