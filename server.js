956bf8639d50710a3c0eb540e94f068bf074abd6
luisvaldes88 <luisvaldes88@gmail.com>
2016-07-23 19:13:37 -0400

    First "mobile" commit. Trigger build.

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ const PORT = 8080;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send(`
-  	<h1>Hello world</h1>
-  	<p>I am running inside a Docker container <b>${os.hostname()}</b></p>.
-  	`);
+  res.send(`
+  	<h1>Hello world</h1>
+  	<p>I am running inside a Docker container <b>${os.hostname()}</b>.</p>
+    <p>This commit is a "mobile" commit.</p>
+  	`);
 });
 
 app.listen(PORT);
