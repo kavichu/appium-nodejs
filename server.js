edbe17fc96cd5810878d85a9cfdb7cec29d25457
luisvaldes88 <luisvaldes88@gmail.com>
2016-07-25 12:39:24 -0400

    Trigger build.

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.get('/', function (req, res) {
   res.send(`
   	<h1>Hello world</h1>
   	<p>I am running inside a Docker container <b>${os.hostname()}</b>.</p>
-    <p>This commit is a "mobile" commit.</p>
+    <p>This commit is a "mobile" commit.</p>
+        <p>Today is 25/07/2016.</p>
   	`);
 });
 
